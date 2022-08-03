09bdf613a9c14f0a62614a95d5258721bddcd6a2
yunho <kimyouknow@naver.com>
2022-08-03 17:50:53 +0900

    Feat: 회원가입 페이지 기본틀 생성

--- frontend/src/pages/Signup/index.tsx
+++ frontend/src/pages/Signup/index.tsx
@@ -1,5 +1,19 @@
+import Box from '@mui/material/Box';
+import TextField from '@mui/material/TextField';
+
+import StackColumn from '@/components/Common/StackColumn';
+
 const SignUp = () => {
-  return <div>SignUp</div>;
+  return (
+    <Box component="form" autoComplete="off">
+      <StackColumn>
+        <TextField id="outlined-basic" label="이메일" variant="outlined" />
+        <TextField id="outlined-basic" label="비빌번호" variant="outlined" />
+        <TextField id="outlined-basic" label="비빌번호확인" variant="outlined" />
+        <button type="submit">회원가입</button>
+      </StackColumn>
+    </Box>
+  );
 };
 
 export default SignUp;
