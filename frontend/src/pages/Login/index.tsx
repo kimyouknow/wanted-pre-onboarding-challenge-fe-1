6b099ae92e210130e80902a1e21f6063d8cd9ed3
yunho <kimyouknow@naver.com>
2022-08-03 17:51:19 +0900

    Feat: 로그인페이지 기본 틀 구성

--- frontend/src/pages/Login/index.tsx
+++ frontend/src/pages/Login/index.tsx
@@ -1,12 +1,17 @@
+import Box from '@mui/material/Box';
+import TextField from '@mui/material/TextField';
+
 import StackColumn from '@/components/Common/StackColumn';
 
 const Login = () => {
   return (
-    <StackColumn>
-      <li>asdfasdf</li>
-      <li>asdfasdf</li>
-      <li>dkdkdk</li>
-    </StackColumn>
+    <Box component="form" autoComplete="off">
+      <StackColumn>
+        <TextField id="outlined-basic" label="이메일" variant="outlined" />
+        <TextField id="outlined-basic" label="비빌번호" variant="outlined" />
+        <button type="submit">로그인</button>
+      </StackColumn>
+    </Box>
   );
 };
 
