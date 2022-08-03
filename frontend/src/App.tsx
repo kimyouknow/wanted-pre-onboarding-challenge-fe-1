9ab7305fb0843ed980bad6d7029b2ca2a047c527
yunho <kimyouknow@naver.com>
2022-08-03 17:50:25 +0900

    Feat: main페이지 제거 후 TODO페이지 생성

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,18 +1,19 @@
-import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import { BrowserRouter as Router, Routes, Route, Navigate } from 'react-router-dom';
 
 import { ROUTE } from '@/constant/route';
 import Layouts from '@/layouts';
 import Login from '@/pages/Login';
-import Main from '@/pages/Main';
 import NotFound from '@/pages/NotFound';
 import SignUp from '@/pages/Signup';
+import Todos from '@/pages/Todos';
 
 const App = () => {
   return (
     <Router>
       <Routes>
         <Route path={ROUTE.MAIN} element={<Layouts />}>
-          <Route index element={<Main />} />
+          <Route index element={<Navigate to={ROUTE.TODO} replace />} />
+          <Route path={ROUTE.TODO} element={<Todos />} />
           <Route path={ROUTE.LOGIN} element={<Login />} />
           <Route path={ROUTE.SIGNUP} element={<SignUp />} />
         </Route>
--- frontend/src/constant/route.ts
+++ frontend/src/constant/route.ts
@@ -1,5 +1,6 @@
 export const ROUTE = {
   MAIN: '/',
+  TODO: '/todo',
   LOGIN: '/login',
   SIGNUP: '/signup',
   FALLBACK: '/*',
