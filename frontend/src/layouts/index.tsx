10f28b6f969c1420f61e0bc9403d76cbe9f965c1
yunho <kimyouknow@naver.com>
2022-08-04 18:13:37 +0900

    Chore: GNB layouts폴더에서 component폴더로 이동

--- frontend/src/layouts/index.tsx
+++ frontend/src/layouts/index.tsx
@@ -2,7 +2,7 @@ import Box from '@mui/material/Box';
 import Container from '@mui/material/Container';
 import { Outlet } from 'react-router-dom';
 
-import GlobalNavigation from '@/layouts/GlobalNavigation';
+import GlobalNavigation from '@/components/GlobalNavigation';
 
 const Layouts = () => {
   return (
