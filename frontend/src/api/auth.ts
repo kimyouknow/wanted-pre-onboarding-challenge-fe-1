ade8a9ba72bc7dd028bd3a08b392e0b69019e4c9
yunho <kimyouknow@naver.com>
2022-08-05 14:09:51 +0900

    Feat: auth api res, req 타입 세팅

--- frontend/src/api/auth.ts
+++ frontend/src/api/auth.ts
@@ -1,18 +1,21 @@
 import authInstance from '@/api/instances/auth.instance';
 import { API } from '@/constant/api';
+import { LoginIngoType, SignUpInfoType } from '@/types/auth';
 
 // TODO: 파라미터 및 body로 넘겨줄 인자 설정하기
 const authApi = {
-  login() {
+  login({ data }: { data: LoginIngoType }) {
     return authInstance({
       url: API.USER.LOGIN,
       method: 'post',
+      data,
     });
   },
-  signUp() {
+  signUp({ data }: { data: SignUpInfoType }) {
     return authInstance({
       url: API.USER.SIGNUP,
       method: 'post',
+      data,
     });
   },
 };
