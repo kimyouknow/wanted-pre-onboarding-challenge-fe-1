80e60acb430fd4b9859ba5ed8462a85dfc4a66d4
yunho <kimyouknow@naver.com>
2022-08-05 17:14:53 +0900

    Feat: useForm

---
+++ frontend/src/hooks/useForm.ts
@@ -0,0 +1,38 @@
+import { ChangeEvent, SyntheticEvent, useState } from 'react';
+
+interface UseFormProps<ValidateProps> {
+  initialValues: ValidateProps;
+  onSubmit: () => void;
+  validate: (values: ValidateProps) => ValidateProps;
+}
+
+const useForm = <ValidateProps>({
+  initialValues,
+  onSubmit,
+  validate,
+}: UseFormProps<ValidateProps>) => {
+  const [inputValues, setInputValues] = useState(initialValues);
+  const [apiError, setApiError] = useState({});
+  const [validateError, setValidateError] = useState(initialValues);
+  const [isLoading, setIsLoading] = useState(false);
+
+  const onChangeHandler = (event: ChangeEvent<HTMLInputElement>) => {
+    const { name, value } = event.target;
+    setInputValues({ ...inputValues, [name]: value });
+    setValidateError(validate({ ...inputValues, [name]: value }));
+  };
+
+  const submitHandler = async (event: SyntheticEvent) => {
+    event.preventDefault();
+    // if (Object.keys(errors).length === 0) {
+    //   onSubmit(values);
+    // }
+    setIsLoading(true);
+    await onSubmit();
+    setIsLoading(false);
+  };
+
+  return { inputValues, validateError, isLoading, onChangeHandler, submitHandler };
+};
+
+export default useForm;
