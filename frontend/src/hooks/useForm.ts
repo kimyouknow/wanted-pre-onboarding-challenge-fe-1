2d80ecc92757b9cd499b279d14e7518e561d5787
yunho <kimyouknow@naver.com>
2022-08-08 00:04:54 +0900

    Refactor: useForm 초기에러값 설정 방식 변경
    validate함수를 이용해 세팅

--- frontend/src/hooks/useForm.ts
+++ frontend/src/hooks/useForm.ts
@@ -13,14 +13,12 @@ const useForm = <ValidateProps>({
 }: UseFormProps<ValidateProps>) => {
   const [inputValues, setInputValues] = useState(initialValues);
   const [apiError, setApiError] = useState({});
-  const [validateError, setValidateError] = useState(initialValues);
+  const [validateError, setValidateError] = useState(validate(initialValues));
   const [isLoading, setIsLoading] = useState(false);
 
   // validateError 객체에 있는 모든 데이터의 value가 ""이거나 null이면 true를 반환
-  // 초기 항목이 "" 빈 값이면 버튼을 활성화하지 않아야하기 때문에 initialValues도 별도로 체크
-  const satisfyAllValidites =
-    Object.values(inputValues).every(value => value !== '') &&
-    Object.values(validateError).every(value => !value);
+
+  const satisfyAllValidites = Object.values(validateError).every(value => !value);
 
   const onChangeHandler = (event: ChangeEvent<HTMLInputElement>) => {
     const { name, value } = event.target;
