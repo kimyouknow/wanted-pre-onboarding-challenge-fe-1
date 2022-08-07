f90c89859082f54a151551f14be5a5515fae153a
yunho <kimyouknow@naver.com>
2022-08-07 22:44:03 +0900

    Refactor: getTodoList 이후 로딩,에러상태 처리 TodoList.tsx로 이동

--- frontend/src/pages/Todo/index.tsx
+++ frontend/src/pages/Todo/index.tsx
@@ -5,45 +5,24 @@ import TodoListProvider, {
   useTodoListProviderState,
 } from '@/context/TodoList';
 import WithProvider from '@/hoc/WithProvider';
+import CreateTodoForm from '@/pages/Todo/Form/Create.TodoForm';
 
 import * as S from './style';
 import TodoDetail from './TodoDetail';
-import TodoElement from './TodoElement';
+import TodoList from './TodoList';
 
-const Todos = () => {
-  const { todoList, isLoading, apiError } = useTodoListProviderState();
-  const { changeTargetTodoId } = useTodoListProviderAction();
-
-  if (isLoading) {
-    return <div>로딩중...</div>;
-  }
-
-  if (apiError.isError) {
-    return (
-      <div>
-        <h3>{apiError.msg}</h3>
-        {/* TODO: 에러 발생시 다시 요청하는 로직  */}
-        <button onClick={() => {}}>다시 요청</button>
-      </div>
-    );
-  }
-
+const Todo = () => {
   return (
     <S.Container>
-      {todoList.length === 0 ? (
-        <div>할 일 목록이 없네요!</div>
-      ) : (
-        <ul>
-          {todoList.map(({ id, ...todoInfo }) => (
-            <TodoElement key={id} todoInfo={{ ...todoInfo, id }} />
-          ))}
-        </ul>
-      )}
-      <Routes>
-        <Route path="/:todoId/" element={<TodoDetail />} />
-      </Routes>
+      <CreateTodoForm />
+      <S.TodoList>
+        <TodoList />
+        <Routes>
+          <Route path="/:todoId/" element={<TodoDetail />} />
+        </Routes>
+      </S.TodoList>
     </S.Container>
   );
 };
 
-export default WithProvider({ Provider: TodoListProvider, Component: Todos });
+export default WithProvider({ Provider: TodoListProvider, Component: Todo });
