d75165d1838aee630ac150f3f5eab4174c2ae2ce
yunho <kimyouknow@naver.com>
2022-08-08 00:34:31 +0900

    Feat: 매요청마다 헤더에 access token 삽입

--- frontend/src/api/instances/api.Instance.ts
+++ frontend/src/api/instances/api.Instance.ts
@@ -1,6 +1,8 @@
-import axios, { AxiosInstance } from 'axios';
+import axios, { AxiosInstance, AxiosRequestConfig } from 'axios';
 
 import { API_URL } from '@/constant/api';
+import { ACCESS_TOKEN } from '@/constant/auth';
+import { handleLocalStorage } from '@/utils/localstorage';
 
 // TODO: headers에 토큰 지정하기
 const apiInstance: AxiosInstance = axios.create({
@@ -8,5 +10,25 @@ const apiInstance: AxiosInstance = axios.create({
   timeout: 3000,
   headers: {},
 });
+
+const setAcessTokenInRequestConfig = (config: AxiosRequestConfig) => {
+  const accessToken = handleLocalStorage.get(ACCESS_TOKEN);
+  if (!config?.headers || !!accessToken) {
+    return config;
+  }
+  config.headers.Authorization = `Bearer ${accessToken}`;
+  return config;
+};
+
+apiInstance.interceptors.request.use(
+  config => {
+    const newConfig = setAcessTokenInRequestConfig(config);
+    return newConfig;
+  },
+  error => {
+    console.error(error);
+    return Promise.reject(new Error('요청 에러 발생'));
+  },
+);
 
 export default apiInstance;
