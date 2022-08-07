a66d7923e314c10468c57f27f8edb3040d544644
yunho <kimyouknow@naver.com>
2022-08-07 13:33:41 +0900

    Mock: 응답 delay1초추가

--- frontend/src/mocks/todoHandler/index.ts
+++ frontend/src/mocks/todoHandler/index.ts
@@ -7,23 +7,23 @@ import { createTodo, deleteTodo, todoDetail, todoList, updateTodo } from './todo
 const todoHandler = [
   // 전체 할 일 목록조회
   rest.get(API_URL + API.TODOS, (req, res, ctx) => {
-    return res(ctx.status(200), ctx.json(todoList));
+    return res(ctx.status(200), ctx.delay(1000), ctx.json(todoList));
   }),
   // id에 해당하는 할 일 디테일 조회
   rest.get(API_URL + API.TODOS + '/:id', (req, res, ctx) => {
-    return res(ctx.status(200), ctx.json(todoDetail));
+    return res(ctx.status(200), ctx.delay(1000), ctx.json(todoDetail));
   }),
   // 할 일 생성
   rest.post(API_URL + API.TODOS + '/:id', (req, res, ctx) => {
-    return res(ctx.status(200), ctx.json(createTodo));
+    return res(ctx.status(200), ctx.delay(1000), ctx.json(createTodo));
   }),
   // 할 일 수정
   rest.put(API_URL + API.TODOS + '/:id', (req, res, ctx) => {
-    return res(ctx.status(200), ctx.json(updateTodo));
+    return res(ctx.status(200), ctx.delay(1000), ctx.json(updateTodo));
   }),
   // 할 일 삭제
   rest.delete(API_URL + API.TODOS + '/:id', (req, res, ctx) => {
-    return res(ctx.status(200), ctx.json(deleteTodo));
+    return res(ctx.status(200), ctx.delay(1000), ctx.json(deleteTodo));
   }),
 ];
 
